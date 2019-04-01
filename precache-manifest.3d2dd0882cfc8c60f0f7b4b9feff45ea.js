c9e981da08c5a9334cd33bb7422252556c1b1e8d
nm <noelmiranda19@gmail.com>
2019-03-31 22:28:58 -1100

    Updates

---
+++ precache-manifest.3d2dd0882cfc8c60f0f7b4b9feff45ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "683f0b681169cc3f2c92",
+    "url": "/noelmiranda/calculator.git/static/js/runtime~main.59cffb98.js"
+  },
+  {
+    "revision": "24c982ece120125c0c83",
+    "url": "/noelmiranda/calculator.git/static/js/main.49c293c6.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/noelmiranda/calculator.git/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "24c982ece120125c0c83",
+    "url": "/noelmiranda/calculator.git/static/css/main.e2a6465e.chunk.css"
+  },
+  {
+    "revision": "3d9df78ca0883a5b30e3f5748ef15640",
+    "url": "/noelmiranda/calculator.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/noelmiranda/calculator.git/precache-manifest.3d2dd0882cfc8c60f0f7b4b9feff45ea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/noelmiranda/calculator.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
